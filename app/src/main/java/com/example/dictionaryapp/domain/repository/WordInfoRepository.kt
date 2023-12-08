28fd4fca516ad480b0fb639f7e81718470eaf49d
davidsbmartins <david.sb.martins@ctw.bmwgroup.com>
2023-12-08 16:03:00 +0000

    Add workthread annotation

--- app/src/main/java/com/example/dictionaryapp/domain/repository/WordInfoRepository.kt
+++ app/src/main/java/com/example/dictionaryapp/domain/repository/WordInfoRepository.kt
@@ -1,9 +1,11 @@
 package com.example.dictionaryapp.domain.repository
 
+import androidx.annotation.WorkerThread
 import com.example.dictionaryapp.core.util.Resource
 import com.example.dictionaryapp.domain.model.WordInfo
 import kotlinx.coroutines.flow.Flow
 
 interface WordInfoRepository {
+    @WorkerThread
     fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>>
 }
